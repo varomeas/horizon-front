4412f6b550f3de166929976df54171a6fa8232db
varomeas <valromeas43@gmail.com>
2024-06-09 13:24:01 -0400

    Carrousel qui affiche les derniers articles

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,21 +1,25 @@
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 function Carousel_1(){
+    const [articles, setArticles] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:8080/api/articles/recent')
+            .then(response => response.json())
+            .then(data => setArticles(data))
+            .catch(error => console.error('Erreur:', error));
+    }, []);
+
     return(
         <Carousel swipeable={true} showThumbs={false} useKeyboardArrows={true} showStatus={false} autoPlay={true} interval={3000} infiniteLoop={true}>
-            <div>
-                <img src={"/images/carousel1.jpg"} />
-
-            </div>
-            <div>
-                <img src={"/images/carousel1.jpg"} />
-                <div className="legend"><p>Lisez notre nouvel article sur les services à domicile </p><button className={"btn"}>Lire</button> </div>
-            </div>
-            <div>
-                <img src={"/images/carousel1.jpg"} />
-                <div className="legend">Legend 3</div>
-            </div>
+            {articles.map((article) => (
+                <div key={article.id}>
+                    <img src={"/images/carousel1.jpg"} />
+                    <div className="legend"><p>{article.title}</p><Link to={`/categorie/transport/article/${article.id}`} className={"btn btn-link"}>Lire</Link></div>
+                </div>
+            ))}
         </Carousel>
     )
 }
