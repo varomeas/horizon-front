ce92faccd80e37d2e5df7e20f3f18e73c23e7a3b
varomeas <valromeas43@gmail.com>
2024-06-16 12:52:11 -0400

    Ajout de la page intergeneration.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -29,7 +29,7 @@ function Menu(){
                 <li><Link to="/categorie/sante">Santé</Link></li>
                 <li><Link to="/categorie/alimentation">Alimentation</Link></li>
                 <li><Link to="/categorie/loisirs">Loisirs</Link></li>
-                <li><Link to="/link6">Inter-génération</Link></li>
+                <li><Link to="/intergeneration">Inter-génération</Link></li>
                 <li><Link to="/categorie/autres">Autres</Link></li>
             </ul>
             <div className={styles.buttons}>
